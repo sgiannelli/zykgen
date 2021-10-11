625dd26ca3703f5d131986bc76810406ff35c363
sgiannelli <90574101+sgiannelli@users.noreply.github.com>
2021-10-12 01:04:54 +0200

    added gen
    usage zykgen -c -l 16 182000000000 182099999999

--- cmd/zykgen/main.go
+++ cmd/zykgen/main.go
@@ -2,25 +2,40 @@ package main
 
 import (
 	"fmt"
-	"github.com/docopt/docopt.go"
-	"github.com/luc10/zykgen"
 	"os"
+	"strconv"cd 
+
+	docopt "github.com/docopt/docopt.go"
+	"github.com/luc10/zykgen"
 )
+
+func replaceAtIndex(in string, r rune, i int) string {
+	out := []rune(in)
+	out[i] = r
+	return string(out)
+}
 
 const usage = `Zyxel VMG8823-B50B WPA Keygen
 
 Usage:
-  zykgen (-m|-n|-c) [-l <length>] <serial>
+  zykgen (-m|-n|-c) [-l <length> -L <letter>] <startserial> <endserial>
   zykgen -h | --help
+  example: usage zykgen -c -l 16 182000000000 182099999999
 
 Options:
   -l <length>     Output key length [default: 10].
+  -L <letter>     Fifth letter of the serial [default: V].
   -h --help       Show this screen.`
 
 func main() {
 	var cocktail zykgen.Cocktail
+	var seriale string
+
 	var args struct {
-		Serial       string `docopt:"<serial>"`
+		Sserial string `docopt:"<startserial>"`
+		Eserial string `docopt:"<endserial>"`
+
+		Letter       string `docopt:"-L"`
 		Length       int    `docopt:"-l"`
 		Mojito       bool   `docopt:"-m"`
 		Negroni      bool   `docopt:"-n"`
@@ -43,5 +58,60 @@ func main() {
 		cocktail = zykgen.Cosmopolitan
 	}
 
-	fmt.Println(zykgen.Wpa(args.Serial, args.Length, cocktail))
+	start, err := strconv.Atoi(args.Sserial)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Serial number should be 12 chars long, (exluding the first char which is 'S'),should contain only numbers, letter 'V' is automatically added")
+		return
+	}
+
+	end, err := strconv.Atoi(args.Eserial)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("Serial number should be 12 chars long, (exluding the first char which is 'S'),should contain only numbers, letter 'V' is automatically added")
+		return
+	}
+
+	if start > end {
+		fmt.Println("End of the serial should be at least > start of the serial")
+		return
+	}
+
+	if len(args.Sserial) != 12 {
+		fmt.Println("Serial number should be 12 chars long, (exluding the first char which is 'S'),should contain only numbers, letter 'V' is automatically added ")
+		return
+	}
+
+	if len(args.Eserial) != 12 {
+		fmt.Println("Serial number should be 12 chars long, (exluding the first char which is 'S'),should contain only numbers, letter 'V' is automatically added  ")
+		return
+	}
+
+	f, err := os.Create("wpa_keys_" + args.Sserial + "_" + args.Eserial + "_" + strconv.Itoa(args.Length) + "_" + args.Letter + "_c.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i := start; i < end; i++ {
+
+		seriale = fmt.Sprintf("%12d", i)
+		seriale = "S" + replaceAtIndex(seriale, []rune(args.Letter)[0], 3)
+		_, err := f.WriteString((zykgen.Wpa(seriale, args.Length, cocktail) + "\n"))
+		if err != nil {
+			fmt.Println(err)
+			f.Close()
+			return
+		}
+
+	}
+
+	fmt.Println("Dictionary of keys was created on the root directory!, now use Hashcat.")
+
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
